2778efe29e52c5934d156ae08dcc88cfbeec96b7
Mohamed Alhayek <mohamedalhayek@Mohameds-MacBook-Pro.local>
2018-04-09 21:44:57 +0200

    add feature to delete message after press the send button

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
-// Make connection
-var socket = io.connect('http://localhost:4000');
+//make conecction
+var socket = io.connect('http://localhost:4000')
 
 // Query DOM
 var message = document.getElementById('message'),
@@ -11,23 +11,23 @@ var message = document.getElementById('message'),
 //Emit events
 
 btn.addEventListener('click', function(){
-    socket.emit('chat',{
+    socket.emit('chat', {
         message: message.value,
         handle: handle.value
     });
+    message.value = '';
 });
 
 message.addEventListener('keypress', function(){
-    socket.emit('typing', handle.value)
+    socket.emit('typing',handle.value)
 })
 
-//listen for events
+//Listen for events
 socket.on('chat', function(data){
-    feedback.innerHTML = "";
-    output.innerHTML += '<p><strong>'+data.handle+':</strong>'+data.message+'</p>';
-
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>'+data.handle+': </strong>'+data.message+'</p>';
 })
 
 socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>'+data+' is typing a message ...<em></p>'
+    feedback.innerHTML = '<p><em>'+data+' is typing a message ..</em></p>'
 })
